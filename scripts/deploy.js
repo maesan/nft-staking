c34dd7f8a84d0ca0820f0baa7f16c26053c27fb1
MAEDA Toshiyuki <maesan@cloud-consulting.asia>
2022-09-21 01:28:13 +0800

    add: deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("deployer: ", deployer.address);
+    const contract = await ethers.getContractFactory("NftStaking");
+    const deployed = await contract.deploy();
+    console.log("contract address: ", deployed.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
+
